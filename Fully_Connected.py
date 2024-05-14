57ef5bb18750ff0337e38063d30487a729fcc5aa
Muhammad Imad Raza <muhammadimadraza@Muhammads-MacBook-Pro.local>
2024-05-14 18:55:37 +0500

    final3

--- Fully_Connected.py
+++ Fully_Connected.py
@@ -18,21 +18,23 @@ class fully_connected:
         return A
     
     def backward_prop(self,learning_rate,Y):
+    
         dl_dw = np.zeros(self.weight.shape) 
-        dl_db = np.zeros(self.bias.shape)
+        dl_db = np.zeros(self.bais.shape)
         dl_back = np.zeros(self.input.shape)
-        print(dl_back.shape)
+        
         for i in range(self.input.shape[2]):
-            output = np.matmul(self.weight,self.input[:,:,i]) + self.bias
+            
+            output = np.matmul(self.weight,self.input[:,:,i]) + self.bais
             Af = Regression.sigmoid(self,output)
 
             dl_dw += np.matmul(Af - Y[i,:], self.input[:,:,i].T)
-            dl_db += np.matmul(Af- Y[i,:],np.ones(self.bias.shape).T)
+            dl_db += np.matmul(Af- Y[i,:],np.ones(self.bais.shape).T)
             dl_back[:,:,i] = (np.matmul(Af - Y[i,:], self.weight)).T
 
         self.weight = self.weight - (learning_rate*dl_dw/Y.shape[1])
-        self.bias = self.bias - (learning_rate*dl_db/Y.shape[1])
-        print(self.weight)
+        self.bais = self.bais - (learning_rate*dl_db/Y.shape[1])
+
         return dl_back
 
     
